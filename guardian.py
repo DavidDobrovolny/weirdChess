9034d27e378f34992a238a85239c68994213f8db
DaDo <daviddobro@volny.cz>
2018-04-16 10:40:21 +0200

    fixed killing guarded pieces

--- guardian.py
+++ guardian.py
@@ -9,7 +9,10 @@ class Guardian(pieceBase.Piece):
         moves = []
 
         for dx, dy in [(x, y) for x in (-1, 0, 1) for y in (-1, 0, 1) if not (x == y == 0)]:
-            killed = () if not game.is_enemy(self.x + dx, self.y + dy, self.color) else (game.board[self.y + dy][self.x + dx],)
+            if game.is_enemy(self.x + dx, self.y + dy, self.color) and game.board[self.y - dy][self.x - dx].is_killable(game):
+                killed = (game.board[self.y + dy][self.x + dx],)
+            else:
+                killed = ()
 
             if len(killed) > 0 and type(killed[0]).__name__ == "Mine":
                 killed = (killed[0], self)
