72faf01a277bd0008ecded476b90ed77d7d365e2
DaDo <daviddobro@volny.cz>
2018-05-17 08:11:58 +0200

    added __str__

--- randomPlayer.py
+++ randomPlayer.py
@@ -5,5 +5,8 @@ class RandomPlayer:
     def __init__(self, num):
         self.number = num
 
+    def __str__(self):
+        return "RandomPlayer"
+
     def choose(self, gameInst):
         return random.choice(gameInst.get_possible_moves())
